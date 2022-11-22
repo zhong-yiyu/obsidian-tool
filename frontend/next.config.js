f0f8b3b93c96df3d88f4eb10898d86dfa1142de7
zhong yiyu <zhong_yi_yu@outlook.com>
2022-11-22 23:50:20 +0000

    添加Nextra的依赖

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.js",
+});
+
+//module.exports = nextConfig
+module.exports = withNextra(nextConfig);
