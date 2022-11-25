60e183fcab09b5a4041dd5d028e465859a6673a8
yiyu zhong <zhong_yi_yu@outlook.com>
2022-11-25 00:21:08 +0000

    添加了CodeBlock功能 v0.1

--- src/main/kotlin/moe/yiyu/obsidian_tool/vault/CodeBlock.kt
+++ src/main/kotlin/moe/yiyu/obsidian_tool/vault/CodeBlock.kt
@@ -1,18 +1,19 @@
 package moe.yiyu.obsidian_tool.vault
 
-class CodeBlock {
+class CodeBlock//        输入的是整个代码块，包含了```和语言标识
+//        remove the code's beginning empty line and ending empty line
+(raw_input: String) {
     var code:String
     var language:String
-    var startLine:Int
-    var endLine:Int
-    constructor(code:String, language:String, startLine:Int, endLine:Int){
-        this.code = code
-        this.language = language
-        this.startLine = startLine
-        this.endLine = endLine
+
+    init {
+        this.language = raw_input.split("\n")[0].replace("```","")
+        val codeTmp = raw_input.replace("```$language","").replace("```","")
+        this.code = codeTmp.split("\n").drop(1).dropLast(1).joinToString("\n")
     }
 
     override fun toString(): String {
-        return "CodeBlock(code='$code', language='$language', startLine=$startLine, endLine=$endLine)"
+        return "CodeBlock(code='$code', language='$language')"
     }
+
 }
